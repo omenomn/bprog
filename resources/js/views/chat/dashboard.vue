8a9276182958d8a06f3b05124c752e52ac82450c
Daniel <daniel@omega323.com>
2018-12-08 18:27:57 +0100

    changes

--- resources/js/views/chat/dashboard.vue
+++ resources/js/views/chat/dashboard.vue
@@ -27,6 +27,7 @@
           <div class="input-group mb-0">
             <input 
               v-on:keyup.enter="send(message)"
+              v-model="message"
               class="form-control form-control-lg message-input" 
               type="text" 
               :placeholder="lang.get('messages.message').capitalize()">  
