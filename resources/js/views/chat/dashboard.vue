6974d4dbccbcd91441bc81dd560e82f4cba5d21c
Daniel <daniel@omega323.com>
2018-12-08 18:15:31 +0100

    fixing mobile input issue

--- resources/js/views/chat/dashboard.vue
+++ resources/js/views/chat/dashboard.vue
@@ -20,6 +20,23 @@
             :interlocutor="interlocutor"></messages>  
         </div>  
       </div>
+          <div class="input-group mb-0">
+            <input 
+              v-on:keyup.enter="send(message)"
+              v-model="message"
+              class="form-control form-control-lg message-input" 
+              type="text" 
+              :placeholder="lang.get('messages.message').capitalize()">  
+            <div class="input-group-append">
+              <button 
+                class="btn btn-outline-secondary" 
+                type="button" 
+                @click="send(message)">
+                <font-awesome-icon 
+                  icon="share-square"/>
+              </button>
+            </div>
+          </div>
     </div>
   </div>
 </template>
